096e6dec01d77bb285f41c054aa99d3f67f11f70
muniz <daniel.muniz997@gmail.com>
2025-05-18 14:31:10 -0300

    lang locale

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'date' => 'O campo :attribute deve ser uma data válida.',
+    'before_or_equal' => 'O campo :attribute deve ser uma data anterior ou igual a hoje.',
+    'after_or_equal' => 'O campo :attribute deve ser uma data posterior ou igual a hoje.',
+    
+    'exists' => 'O :attribute selecionado é inválido.',
+    
+    'attributes' => [
+        'start_date' => 'data de retirada',
+        'due_date' => 'data de devolução',
+        'user_id' => 'usuário',
+        'book_id' => 'livro',
+    ],
+];
